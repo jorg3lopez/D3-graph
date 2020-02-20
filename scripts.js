07e4969d31da49c57165733da5a36ac91abb1d1c
jorg3lopez <49923512+jorg3lopez@users.noreply.github.com>
2020-02-19 23:44:59 -0800

    ready for backend

--- scripts.js
+++ scripts.js
@@ -7,23 +7,35 @@ var employees=[];
    document.getElementById('button-submit').addEventListener('click', send);
    
 }
+  function toServer (dataJson){
+     /*only if I need to manually send the JSON file to the server.*/
+   }
 function getData() {
   'use strict';
    let employeeId = Math.floor((Math.random() * 99999999) + 10000000);
-   let employee = {employeeId: employeeId};
-   let employeeJson = JSON.stringify(employee);
+   let employee;
+   let employeeJson;
    let firstN = document.getElementById('firstname').value;
    let lastN = document.getElementById('lastname').value;
    let department = document.getElementById('department').value;
    let keepgoing = true;
+   let date = new Date();
+   let month = month_name(date);
+   let hireDate = month + " " + date.getDate() + " " + date.getFullYear();
       
   while(keepgoing) {
     if(employees.length === 0) {
-      employees[employees.length] = {firstName: firstN, lastName: lastN, department: department, Id: employeeId};
+      employees[employees.length] = {firstName: firstN, lastName: lastN, department: department, Id: employeeId, hireDate: hireDate};
+      employee = {firstName: firstN, lastName: lastN, department: department, Id: employeeId, hireDate: hireDate};
+      employeeJson = JSON.stringify(employee);
       keepgoing = false;
+      toServer(employeeJson);
     }
     else if(employees.indexOf(employeeId) == -1) {
-    employees.push({firstName: firstN, lastName: lastN, department: department, Id: employeeId});
+    employees.push({firstName: firstN, lastName: lastN, department: department, Id: employeeId, hireDate: hireDate});
+    employee = {firstName: firstN, lastName: lastN, department: department, Id: employeeId, hireDate: hireDate};
+    employeeJson = JSON.stringify(employee);
+    toServer(employeeJson);
     keepgoing = false;
     }
     else {
@@ -48,16 +60,17 @@ let mlist = [ "January", "February", "March", "April", "May", "June", "July", "A
       //do nothing
    }
    else {
-     let date = new Date();
-   let month = month_name(date);
+     //let date = new Date();
+   //let month = month_name(date);
    getData();
   document.getElementById('out-header').innerHTML = "<h1>Employee added</>";
   document.getElementById('out-name').innerHTML = "Name: " + employees[employees.length-1].firstName + ", " + employees[employees.length-1].lastName;
   document.getElementById('out-department').innerHTML = "Department: " + employees[employees.length-1].department;
   document.getElementById('out-employeeid').innerHTML = "Employee ID: " + employees[employees.length-1].Id;
-  document.getElementById('out-hiredate').innerHTML = "Hire Date: " + month + " " + date.getDate() + " " + date.getFullYear();
+  document.getElementById('out-hiredate').innerHTML = "Hire Date: " + employees[employees.length-1].hireDate;
   document.getElementById('out-totalemployees').innerHTML = "Total Employees: " + employees.length;
    }
+ 
    
 }
 //document.addEventListener('DOMContentLoaded', init);
